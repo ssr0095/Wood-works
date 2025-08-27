efb48a4ad4dbdafcfc2e5fbb46563868ee347f6f
Sriram S <ssrssr0095@gmail.com>
2025-08-27 19:34:38 +0530

    fix: eslint disabled

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
